f5d8749167a3a8698d38fbf62220d18568f1ed4f
havvanuraydin <havvanuraydiin@gmail.com>
2020-05-20 18:00:37 +0200

    add Practice.java

---
+++ src/tests/Practice.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        for (int i = 1; i<6 ; i++){
+            System.out.println("HEY! YOU DO EVERYTHING WELL!");
+        }
+    }
+}
