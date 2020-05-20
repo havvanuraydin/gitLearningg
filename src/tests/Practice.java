9e8f26f2534867d8da02d67ca1858e528fa9ee99
havvanuraydin <havvanuraydiin@gmail.com>
2020-05-20 18:14:35 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -5,6 +5,7 @@ public class Practice {
     public static void main(String[] args) {
         for (int i = 1; i<6 ; i++){
             System.out.println("HEY! YOU DO EVERYTHING WELL!");
+            System.out.println("BECAUSE YOU ARE PERFECT!");
         }
     }
 }
